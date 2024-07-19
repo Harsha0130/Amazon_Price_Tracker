a6ef0a9f65b3e4fd1af4a862c39a838a800de9c3
Harsha <harshahm0130@gmail.com>
2024-07-19 22:28:57 +0530

    Sending alert mail if the target price trigger

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ soup = BeautifulSoup(response.text, "html.parser")
 price = soup.find(name="span", class_="aok-offscreen").getText()
 float_price = float(price.replace(" $", ""))
 
-print(float_price)
+title = soup.find(name="span", id="productTitle")
+title = ' '.join(title.getText().strip().split())
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=EMAIL, password=PASSWORD)
-    connection.sendmail(from_addr=EMAIL,
-                        to_addrs=TO_ADDRESS,
-                        msg="Hello")
+TARGET_PRICE = 100
 
+if float_price < TARGET_PRICE:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=TO_ADDRESS,
+                            msg=f"Subject:Amazon Price Alert!\n\n{title} is now at {price}\nBuy it here: {URL}".encode("utf-8"))
