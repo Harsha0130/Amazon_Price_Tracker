dbdbbb6dc1e89592df3ff6a9af1685a2100572b8
Harsha <harshahm0130@gmail.com>
2024-07-19 22:11:17 +0530

    Setting up the SMTP library

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+import smtplib
 
-URL = ("https://appbrewery.github.io/instant_pot/")
+URL = ("https://appbrewery.github.io/instant_pot")
+EMAIL = os.environ["EMAIl"]
+PASSWORD = os.environ["PASSWORD"]
+TO_ADDRESS = os.environ["TO_ADDRESS"]
+
 
 response = requests.get(url=URL)
 
@@ -10,6 +16,12 @@ soup = BeautifulSoup(response.text, "html.parser")
 price = soup.find(name="span", class_="aok-offscreen").getText()
 float_price = float(price.replace(" $", ""))
 
-print(type(float_price))
+print(float_price)
 
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=EMAIL, password=PASSWORD)
+    connection.sendmail(from_addr=EMAIL,
+                        to_addrs=TO_ADDRESS,
+                        msg="Hello")
 
