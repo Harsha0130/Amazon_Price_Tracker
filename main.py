6ba7c81ab274e954ab206851d2cee3fdbbc52f07
Harsha <harshahm0130@gmail.com>
2024-07-19 22:52:09 +0530

    Setting up the header and scraping the live site

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import requests
 import os
 import smtplib
 
-URL = ("https://appbrewery.github.io/instant_pot")
+URL = ("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
 EMAIL = os.environ["EMAIl"]
 PASSWORD = os.environ["PASSWORD"]
 TO_ADDRESS = os.environ["TO_ADDRESS"]
 
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
 
-response = requests.get(url=URL)
+
+response = requests.get(url=URL, headers=header)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
